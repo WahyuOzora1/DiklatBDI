67b9d4fce9d0932f28dddb5569c1231404248bd5
Wahyu Restu Pamuji <68573221+WahyuOzora1@users.noreply.github.com>
2021-11-08 08:00:53 +0800

    Update array1.php

--- array1.php
+++ array1.php
@@ -1,4 +1,14 @@
 <?php
+	// $star=3;
+	// for( $a=$star;$a>0;$a--){
+	// for($i=1; $i<=$a; $i++){
+	// 	echo "0";
+	// }
+	// for($a1=$star;$a1>=$a;$a1--){
+	// 	echo"*";
+	// }
+	// echo"<br>";
+	// }
 
 $kabupaten = array(
     array("Kabupaten Badung", "Badung"),
@@ -27,10 +37,15 @@ echo $kabupaten[3][0] . " pusat pemerintahannya berada di " . $kabupaten[3][1] .
 echo $kota[0][0]." Pusat Pemerintahannya " .$kota[0][1]."<br>"; 
 echo "================================================ <br>";
 
-for($row = 0; $row<$totalKabupaten; $row++){
-    echo " <br> Data Kabupaten di Provinsi Bali <br>";
-    echo "============== <br>";
-    for ($col=0; $col < 2; $col++) { 
-    echo $kabupaten[$row][$col];
-    }
+// for($row = 0; $row<$totalKabupaten; $row++){
+//     echo " <br> Data Kabupaten di Provinsi Bali <br>";
+//     echo "============== <br>";
+//     for ($col=0; $col < 2; $col++) { 
+//     echo $kabupaten[$row][$col];
+//     }
+// }
+
+
+for ($i=0; $i <$totalKabupaten ; $i++) { 
+	echo $kabupaten[$i][0]." Pusat pemerintahannya di ". $kabupaten[$i][0] ."<br>";
 }
